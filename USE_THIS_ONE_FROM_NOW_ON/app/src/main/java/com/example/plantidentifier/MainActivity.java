c782e9204729141afec5a1d5eff22e39d67a74f5
CelestialSloth <52363369+CelestialSloth@users.noreply.github.com>
2019-07-23 14:29:24 -0700

    Hopefully "Identify Plants" button works

--- USE_THIS_ONE_FROM_NOW_ON/app/src/main/java/com/example/plantidentifier/MainActivity.java
+++ USE_THIS_ONE_FROM_NOW_ON/app/src/main/java/com/example/plantidentifier/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.plantidentifier;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
@@ -12,5 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button goToCamera = (Button) findViewById(R.id.toCamera);
+        goToCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Camera.class);
+                startActivity(intent);
+            }
+
+        });
     }
 }
