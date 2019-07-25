32da6c9a2ce9a4c6f32ce13ace0be062edaa4bb0
B <brontemckinnis@gmail.com>
2019-07-25 11:10:00 -0700

    updated camera page

--- USE_THIS_ONE_FROM_NOW_ON/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
+++ USE_THIS_ONE_FROM_NOW_ON/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/constraintlayout/widget/R.java
@@ -82,11 +82,11 @@ public final class R {
         public static final int parent = 0x7f080074;
         public static final int percent = 0x7f080077;
         public static final int right = 0x7f08007e;
-        public static final int spread = 0x7f0800a1;
-        public static final int spread_inside = 0x7f0800a2;
-        public static final int start = 0x7f0800a7;
-        public static final int top = 0x7f0800c2;
-        public static final int wrap = 0x7f0800d1;
+        public static final int spread = 0x7f0800a2;
+        public static final int spread_inside = 0x7f0800a3;
+        public static final int start = 0x7f0800a8;
+        public static final int top = 0x7f0800c3;
+        public static final int wrap = 0x7f0800d2;
     }
     public static final class styleable {
         private styleable() {}
