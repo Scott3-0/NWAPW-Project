2a49e79b2fb3a4fee9c0aa1adddeb96986d76195
Julissa <julissakochi416@gmail.com>
2019-07-25 11:29:48 -0700

    Changed "camera" again

--- USE_THIS_ONE_FROM_NOW_ON/app/src/main/java/com/example/plantidentifier/Camera.java
+++ USE_THIS_ONE_FROM_NOW_ON/app/src/main/java/com/example/plantidentifier/Camera.java
@@ -4,17 +4,29 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class Camera extends AppCompatActivity {
+
+    private static final String TAG = "ChoosePictureActivity";
 
     private static final int PERMISSION_CODE = 0;
 
@@ -50,6 +62,7 @@ public class Camera extends AppCompatActivity {
     private void pickImageFromGallery() {
         Intent intent = new Intent(Intent.ACTION_PICK);
         intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
         startActivityForResult(intent, 2);
     }
 
@@ -69,7 +82,15 @@ public class Camera extends AppCompatActivity {
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         ImageView imageView = findViewById(R.id.image_view);
         if (resultCode == RESULT_OK && requestCode == 2){
-            imageView.setImageURI(data.getData());
+            Uri imageUri = data.getData();
+            ContentResolver contentResolver = getContentResolver();
+            try {
+                InputStream inputStream = contentResolver.openInputStream(imageUri);
+                Bitmap bitmap = BitmapFactory.decodeStream(inputStream);
+                imageView.setImageBitmap(bitmap);
+            } catch(FileNotFoundException e) {
+                Log.e(TAG, e.getMessage(), e);
+            }
         }
     }
 
