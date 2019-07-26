4672f8ef08453317817319afc9bcf7745c464fe9
Julissa <julissakochi416@gmail.com>
2019-07-26 12:16:37 -0700

    Not sure

--- USE_THIS_ONE_FROM_NOW_ON/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/fragment/R.java
+++ USE_THIS_ONE_FROM_NOW_ON/app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/fragment/R.java
@@ -113,6 +113,7 @@ public final class R {
         public static final int right = 0x7f08007e;
         public static final int right_icon = 0x7f08007f;
         public static final int right_side = 0x7f080080;
+<<<<<<< Updated upstream
         public static final int start = 0x7f0800a8;
         public static final int tag_transition_group = 0x7f0800ad;
         public static final int tag_unhandled_key_event_manager = 0x7f0800ae;
@@ -122,6 +123,17 @@ public final class R {
         public static final int time = 0x7f0800bd;
         public static final int title = 0x7f0800be;
         public static final int top = 0x7f0800c3;
+=======
+        public static final int start = 0x7f0800a7;
+        public static final int tag_transition_group = 0x7f0800ac;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800ad;
+        public static final int tag_unhandled_key_listeners = 0x7f0800ae;
+        public static final int text = 0x7f0800b0;
+        public static final int text2 = 0x7f0800b1;
+        public static final int time = 0x7f0800bc;
+        public static final int title = 0x7f0800bd;
+        public static final int top = 0x7f0800c2;
+>>>>>>> Stashed changes
     }
     public static final class integer {
         private integer() {}
